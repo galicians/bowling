9bffcfe0e62e103a099fd55f4c633e0843e3cdb1
galicians <galicians@gmail.com>
2014-10-17 16:51:03 +0100

    game accepts one player tested

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,43 +10,73 @@
 // 	gutter game(0) vs perfect game(300 pints)
 // 	30 points as max / frame
 
-describe("game", function() {
+describe("To play a bowling game we need", function() {
 	
 
 
-	describe("setting up the playing area", function() {
+	describe("set up the playing area", function() {
+
+		var game, 
+			pin, 
+			player, 
+			frame;
+
+			beforeEach(function() {
+				game = new Game
+				pin = new Pin
+				player = new Player
+				frame = new Frame
+			})
+
 
 		it("should be defined", function() {
-			var game = new Game
 			expect(game).toBeDefined()
 		})
 
 		it("should have pins", function() {
-			var pin = new Pin
 			expect(pin).toBeDefined()
 		})
 
 		it("should have a player", function() {
-			var player = new Player
 			expect(player).toBeDefined()
 		})
 
+		it("should be able to accept a player", function() {
+			game.addPlayer(player)
+			expect(game.player).toEqual(player)
+		})
+
 		it ("should have frames", function() {
-			frame = new Frame
 			expect(frame).toBeDefined()
 		})
 
+		it("initial number of rolls per frame is should be two", function() {
+			expect(frame.roll).toEqual(2)
+		})
+
 		it("the number of frames per game by default is 10", function() {
-			var game = new Game
 			expect(game.frames.length).toEqual(10)
 		})
 
 		it("the number of inital pins at the begining of a frame is 10", function() {
-			var frame = new Frame
 			expect(frame.pins.length).toEqual(10)	
 		})
 
-		
+		it("each player should have a initial score of cero points", function() {
+			expect(player.score).toBeDefined
+		})
+
+
+	})
+
+	describe("the rules of the bowling game", function() {
+
+		it("after a roll you should be able to knock down pins", function() { 
+			// Game.player.rollsBall
+
+			// expect(player.rollsBall).
+		})
+
 
 	})
 })
--- src/game.js
+++ src/game.js
@@ -1,14 +1,21 @@
 function Game() {
 	this.frames = []
 	for(var i =0; i < 10; i++ ) { this.frames.push(new Frame) }
+	this.player
 }
 
 function Pin() {}
 
-function Player() {}
+function Player() {
+	this.score = 0
+}
 
 function Frame() {
 	this.pins = []
+	this.roll = 2
 	for(var i=0; i < 10; i++ ) { this.pins.push(new Pin) }
 }
 
+Game.prototype.addPlayer = function(player) {
+	this.player = player
+}
